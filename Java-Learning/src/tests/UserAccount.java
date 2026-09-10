d9724bbeac743574d4e42ecfd3d10190636e53a6
Caua Matheus Habermann Pereira <cauahabermann7@gmail.com>
2026-09-10 19:17:25 -0300

    Adiciona alteracoes do UserAccount

--- Java-Learning/src/tests/UserAccount.java
+++ Java-Learning/src/tests/UserAccount.java
@@ -2,90 +2,54 @@ package tests;
 
 public class UserAccount {
     private String email;
-    private String userName;
+    private String UserName;
     private UserAccount[] followers;
     private Post[] timeline = new Post[10];
     private Post[] posts;
 
-    public UserAccount(String email, String userName){
+
+    public UserAccount(String email, String UserName) {
         this.email = email;
-        this.userName = userName;
-        this.followers = new UserAccount[10];
-        this.timeline = new  Post[10];
+        this.UserName = UserName;
+        this.followers = new UserAccount[999];
+        this.timeline = new Post[10];
         this.posts = new Post[10];
     }
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public void updateTimeline(Post newPost) {
+    public void updateTimeline (Post newPost) {
         for (int i = 0; i < timeline.length; i++) {
             if (timeline[i] == null) {
                 timeline[i] = newPost;
                 return;
             }
         }
-        for(int i = 0;i < timeline.length - 1; i ++){
+        for (int i = 0; i < timeline.length - 1; i++) {
             timeline[i] = timeline[i + 1];
         }
         timeline[posts.length - 1] = newPost;
     }
 
+
     public void publish(String quote){
-        Post newPost = new Post(this, quote);
-        for(int i = 0; i < posts.length; i ++){
+    if(quote == null) return;
+
+    Post newPost = new Post(this, quote);
+    for (int i = 0; i < posts.length; i++) {
             if(posts[i] == null){
                 posts[i] = newPost;
                 break;
-
             }
-        }
-        for(UserAccount follower:followers){
-            follower.updateTimeline(newPost);
-        }
+    }
+    updateTimeline(newPost);
+
     }
 
-    public boolean delete(int postidx){
-        if(postidx < 0 || postidx >= posts.length || posts[postidx] == null ) return false;
-        for(int i = postidx;i < posts.length - 1; i ++){
-            posts[i] = posts[i + 1];
-        }
-        posts[posts.length - 1] = null;
-        return true;
-    }
-    
-    public void booPost(int postidx){
-        if(postidx >= 0 && postidx < timeline.length ) timeline[postidx].boo();
-    }
 
-    public void clapPost(int postidx){
-        if(postidx >= 0 && postidx < timeline.length ) timeline[postidx].clap();
-    }
 
-    public void acceptFollower(UserAccount newFollower){
-        if(newFollower == null) return;
-        for(UserAccount follower:followers){
-                if(follower.equals(newFollower)) return;
-        }
-        for(int i = 0;i < followers.length; i++){
-            if(followers[i] == null){
-                followers[i] = newFollower;
-                return;
-            }
-        }
-    }
-
-    public void blockFollower(UserAccount follower){
-        if(follower == null) return;
-        for(int i = 0; i < followers.length; i++){
-            if(followers[i].equals(follower)){
-                for(int j = i; j < followers.length - 1; j ++){
-                    followers[j] = followers[j + 1];
-                }
-            }
-        }
-        followers[followers.length - 1] = null;
-    }
 
 }
+
+
+
+
+
